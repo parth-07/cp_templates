acabff0713ebbf8fa2a96279089ac42a241fbcd4
Parth <partharora99160808@gmail.com>
2020-07-28 00:50:54 +0530

    Updated nearest power of 2 algo

--- algorithms/number_theory/NearestPow2.cpp
+++ algorithms/number_theory/NearestPow2.cpp
@@ -1,15 +1,11 @@
 #include<bits/stdc++.h>
-#define show(x) cout<<#x" = "<<x<<endl;
 #define INF 0x3f3f3f3f
-using namespace std;
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-typedef pair<int,int> iipair;
-typedef pair<unsigned,unsigned> uupair;
-typedef vector<int> vec;
-typedef vector<unsigned> uvec;
 
 ll powerof2(ll n)
 {
@@ -23,8 +19,6 @@ ll powerof2(ll n)
 }
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
     ll n;
     cin>>n;
     cout<<"nearest power of 2 less than or equal to n = "<<powerof2(n)<<endl;
